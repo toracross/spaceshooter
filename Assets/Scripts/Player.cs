80b2c897269bf5e7d43f6734aea5c7705cb4d943
Wellison Pereira <wellison.pereira@jassby.com>
2019-02-03 22:58:12 -0500

    Player Movement System

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //Variables
+    [SerializeField] private float speed = 5.0f;
+
+    //Start is called before the first frame update
+    private void Start() {
+       
+    }
+
+    //Update is called once per frame
+    private void Update() {
+        Movement();
+    }
+
+    private void Movement() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * speed * horizontal * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * vertical * Time.deltaTime);
+
+        if (transform.position.y > 0) {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        } else if (transform.position.y < -4.2f) {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+        if (transform.position.x > 9.5f) {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0);
+        } else if (transform.position.x < -9.5f) {
+            transform.position = new Vector3(9.5f, transform.position.y, 0);
+        }
+    }
+}
