7b6d7033f071536a16a5a76b4a8e10ad4bc75e72
Wellison Pereira <wellison.pereira@icloud.com>
2019-02-04 13:37:21 -0500

    SpawnManager - Spawn enemies and powerups

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    [SerializeField] private GameObject enemyShipPrefab;
+    [SerializeField] private GameObject[] powerups;
+
+    // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(EnemySpawnRoutine());
+        StartCoroutine(PowerupSpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    //Enemy Spawner
+    IEnumerator EnemySpawnRoutine() {
+        while (true) {
+            int randomRange = Random.Range(-7, 7);
+
+            Instantiate(enemyShipPrefab, new Vector3(randomRange, 7, 0), Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+
+    IEnumerator PowerupSpawnRoutine() {
+        while (true) {
+            int randomPowerup = Random.Range(0, 2);
+            int randomRange = Random.Range(-7, 7);
+
+            Instantiate(powerups[randomPowerup], new Vector3(randomRange, 7, 0), Quaternion.identity);
+            yield return new WaitForSeconds(10.0f);
+        }
+    }
+}
